06e25425857348eab39c278eaaae13080036f0a9
JAlexander94 <118635922+JAlexander94@users.noreply.github.com>
2023-02-08 21:07:17 +0000

    made averaging function for house prices and pulled out the lon and lat boundaries in a variable called latlon for use in the police api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 
-var queryURL = "https://data.police.uk/api/crimes-street-dates"
+var queryURL = "https://data.police.uk/api/leicestershire/NC04"
+let latlon
 
 $.ajax({
     url: queryURL,
@@ -11,7 +12,7 @@ $.ajax({
 const settings = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://zoopla.p.rapidapi.com/house-prices/estimate?area=e1w&ordering=descending&page_number=1&page_size=40",
+	"url": "https://zoopla.p.rapidapi.com/house-prices/estimate?area=tower%20hamlets&ordering=descending&page_number=1&page_size=40",
 	"method": "GET",
 	"headers": {
 		"X-RapidAPI-Key": "4a8d4a065emsh408cdcdfe8a5c30p157467jsnfc37494fcb57",
@@ -19,13 +20,26 @@ const settings = {
 	}
 };
 
+//call the zoopla api via rapidapi for the area in the url above and average the house prices
 $.ajax(settings).then(function (response) {
 	console.log(response);
     var houseprices = []
     for(let i=0;i<response.property.length;i++){
         var price = response.property[i].estimate_value
         if(price>0){houseprices.push(price)}
-        
     }
+    let averageprice = average(houseprices).toFixed(0)
+    latlon = response.bounding_box
     console.log(houseprices)
+    console.log(averageprice)
+    console.log(latlon)
 });
+
+// function to average arrays
+function average(arr){
+    let sum = 0
+    for (let i = 0;i<arr.length;i++){
+        sum+= arr[i]
+    }
+    return sum / arr.length
+}
